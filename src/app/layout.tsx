befb9b53d5a8da342bbdcd888c76ae361d1ea690
Priyanshu Chaudhary <priyanshuchaudhary53@gmail.com>
2024-02-01 05:03:59 +0530

    Add @vercel/speed-insights package and component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Header from "@/components/Layout/Header";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className + " dark:bg-[#121212]"}>
         <Header />
         <main>{children}</main>
+        <SpeedInsights />
       </body>
     </html>
   );
